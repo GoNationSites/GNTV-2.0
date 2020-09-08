704f4f1072ab61ea4add3767770d18b875cd3582
Eric Smith <ericsmith.100@gmail.com>
2020-09-08 08:34:42 -0400

    Add price to contentblock component

--- src/components/contentBlock.js
+++ src/components/contentBlock.js
@@ -2,10 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 const ContentBlock = ({ item }) => {
+  const price = item.variants.length ? item.variants[0].price : 0
+  // todo: render price variants. ATM we only render the first price inside of the variants array
+  const renderPrice = () => (price ? price : '')
   return (
     <Block>
       <Title>{item.name}</Title>
       <Description>{item.desc}</Description>
+      <Price>{renderPrice()}</Price>
     </Block>
   )
 }
@@ -28,5 +32,10 @@ const Title = styled.h1`
 const Description = styled.p`
   font-size: 1.25rem;
   line-height: 2rem;
+  text-align: left;
+`
+
+const Price = styled.p`
+  font-size: 1.25rem;
   text-align: left;
 `
