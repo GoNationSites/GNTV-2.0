41ca82eb1f301176bc71ec4498c280c93cbe4a11
Eric Smith <ericsmith.100@gmail.com>
2020-09-18 13:44:33 -0400

    Add 2 new TV's

---
+++ dsb-c/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // gonationID='bzn-XaUXGg1OScOoINf6ICrrUA'
+      texture='https://res.cloudinary.com/gonation/image/upload/v1599581888/GNTV/spotted-horse-shelton-texture.jpg'
+      plID='7'
+      // tvID='gntv--MzCwN76Qu6-yX4CDOREcg'
+
+      gonationID='bzn-yO3xgUsKQCS7GWg0Q2ewbQ'
+      tvID='gntv-OreesZEMQS2c7YaPd_WWZQ'
+    />
+  )
+}
+
+export default App
