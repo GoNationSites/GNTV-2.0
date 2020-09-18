545e5afbcac0c853051fa78a4d1d55378674d837
Eric Smith <ericsmith.100@gmail.com>
2020-09-18 14:14:38 -0400

    Configure spotted horse TV

---
+++ spotted-horse/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // gonationID='bzn-XaUXGg1OScOoINf6ICrrUA'
+      texture='https://res.cloudinary.com/gonation/image/upload/v1599581888/GNTV/spotted-horse-shelton-texture.jpg'
+      plID='1'
+      // tvID='gntv--MzCwN76Qu6-yX4CDOREcg'
+
+      gonationID='bzn-XaUXGg1OScOoINf6ICrrUA'
+      tvID='gntv-7Y53CK-ESXa9bMUTyo9ZpQ'
+    />
+  )
+}
+
+export default App
