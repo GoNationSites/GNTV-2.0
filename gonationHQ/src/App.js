f3fb637abe36aab2d2bdb71b835e7bd09a67dc3b
esmith7196 <ericsmith.100@gmail.com>
2020-10-10 12:54:02 -0400

    Add gonation-hq TV

---
+++ gonationHQ/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // gonationID='bzn-XaUXGg1OScOoINf6ICrrUA'
+      texture='https://res.cloudinary.com/gonation/image/upload/v1602348121/GNTV/gonation-hq-texture.jpg'
+      plID='1'
+      // tvID='gntv--MzCwN76Qu6-yX4CDOREcg'
+
+      gonationID='bzn-YP2dYnuPTQyB9BBT-WnGxA'
+      tvID='gntv-qgjCcd56Q-_d4p1uEUcY2w'
+    />
+  )
+}
+
+export default App
