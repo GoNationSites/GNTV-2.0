9724e7d3609d2a063c95d55a8b5a1830eb1ad27b
esmith7196 <ericsmith.100@gmail.com>
2020-10-10 13:04:49 -0400

    Implement Que TV 1

---
+++ que-a/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // gonationID='bzn-XaUXGg1OScOoINf6ICrrUA'
+      texture='https://www.qwhiskeykitchen.com/images/drinks-bg.jpg'
+      plID='8'
+      // tvID='gntv--MzCwN76Qu6-yX4CDOREcg'
+
+      gonationID='bzn-RX6J48-DRp_PyZSrURTQBQ'
+      tvID='gntv-H-aAn3JVRf6DmDIrHPy9Ag'
+    />
+  )
+}
+
+export default App
