be9e38313a2a3bf2e955c677a3875df2ad191a0c
esmith7196 <ericsmith.100@gmail.com>
2020-11-10 10:56:10 -0500

    Add La Picante TV A

---
+++ la-picante-a/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      texture='"https://www.transparenttextures.com/patterns/ag-square.png"'
+      plID='1'
+      gonationID='bzn-4LVvOm-bR2i9TE2wDplhoA'
+      tvID='gntv-Qk52KxNgSoC01dOPoLBvRQ'
+    />
+  )
+}
+
+export default App
