8ebb98169526401d53ddd15f0f4d16914857c486
esmith7196 <ericsmith.100@gmail.com>
2020-12-09 16:36:37 -0500

    [skip ci] change tv id for b

--- roman-b/src/App.js
+++ roman-b/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       texture='https://res.cloudinary.com/gonation/image/upload/v1606923661/GNTV/RomeTV4.jpg'
       plID='5'
       gonationID='bzn-Yi5jWTSiR1qvn3_7S2aYsg'
-      tvID='gntv-f0AaWlIlRu_ktuCEr0q89A'
+      tvID='gntv-Mu_7s6PpRUmxGyhyMWlUQg'
     />
   )
 }
