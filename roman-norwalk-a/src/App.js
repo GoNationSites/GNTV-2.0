81acf8b3a6272695b6e5984acbca1583e380b1fd
esmith7196 <ericsmith.100@gmail.com>
2021-03-01 12:08:18 -0500

    [skip ci] Add roman norwalk showcase tv

---
+++ roman-norwalk-a/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+// import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // listConfig={{ listConfiguration }}
+      texture='https://res.cloudinary.com/gonation/image/upload/v1606923661/GNTV/RomeTV4.jpg'
+      plID='1'
+      gonationID='5730f6077455de63b3fe94dd'
+      tvID='gntv-IAljSTNpSWuiQ8P0ii2d9A'
+    />
+  )
+}
+
+export default App
