379573c1cc00683655022f393d9de47a9a9dd2a1
esmith7196 <ericsmith.100@gmail.com>
2021-03-01 12:02:26 -0500

    Add Romanacci Westport showcase TV

---
+++ roman-westport-a/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+// import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // listConfig={{ listConfiguration }}
+      texture='https://res.cloudinary.com/gonation/image/upload/v1606923661/GNTV/RomeTV4.jpg'
+      plID='1'
+      gonationID='59f0f12f7455de309071b4d7'
+      tvID='gntv--ZgVeionR2q-yENVd8n6hg'
+    />
+  )
+}
+
+export default App
