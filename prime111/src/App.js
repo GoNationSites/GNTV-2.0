96b9c6cb179a882d841a05e3d79f3d7538aa173b
esmith7196 <ericsmith.100@gmail.com>
2021-03-18 12:40:31 -0400

    [skip ci] Add prime 111

---
+++ prime111/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { TV } from 'gntv-component'
+// import 'gntv-component/dist/index.css'
+
+const App = () => {
+  return (
+    <TV
+      // listConfig={{ listConfiguration }}
+      texture=''
+      plID='6'
+      gonationID='514fa42cb111aa1572000000'
+      tvID='gntv-fTh00zimRG6yg6BADy76Ig'
+    />
+  )
+}
+
+export default App
