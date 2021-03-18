a595093b628c61ac98d4701cd4145fb1daa70cbd
esmith7196 <ericsmith.100@gmail.com>
2021-03-18 12:52:22 -0400

    [skip ci] Build

--- prime111/src/App.js
+++ prime111/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <TV
       // listConfig={{ listConfiguration }}
-      texture=''
+      texture='https://res.cloudinary.com/gonation/image/upload/v1616086286/GNTV/prime_thing.jpg'
       plID='6'
       gonationID='514fa42cb111aa1572000000'
       tvID='gntv-fTh00zimRG6yg6BADy76Ig'
